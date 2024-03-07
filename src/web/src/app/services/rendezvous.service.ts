6f742e8dfaa0a57eaf2ee29146a4b945851be0f8
oma710 <hamza.omaddi@gmail.com>
2024-03-07 18:00:39 +0100

    render vous service for api

--- src/web/src/app/services/rendezvous.service.ts
+++ src/web/src/app/services/rendezvous.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TimeSlot } from '../model/TimeSlot';
+import {Rendezvous} from "../model/rendezvous";
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,10 @@ export class RendezvousService {
   constructor(private http:HttpClient) { }
 
   private readonly urlApi = 'http://localhost:8080/rendezvous';
+
+  saveRendezvous(rendezvous: Rendezvous){
+    return this.http.post(this.urlApi+"/save",rendezvous)
+  }
 
   getAllTime():Observable<TimeSlot[]>{
     return this.http.get<TimeSlot[]>(this.urlApi+"/time")
