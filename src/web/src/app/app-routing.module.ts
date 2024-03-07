d20fbce137812ac19f3b5aa278e47ea558076dd6
oma710 <hamza.omaddi@gmail.com>
2024-03-07 18:17:22 +0100

    remove medecin compnent from router

--- src/web/src/app/app-routing.module.ts
+++ src/web/src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RendezvousComponent } from './rendezvous/rendezvous/rendezvous.component';
 import { PatientComponent } from './rendezvous/patient/patient.component';
-import { MedecinComponent } from './medecin/medecin.component';
 
 const routes: Routes = [
   {path:"patient", component:PatientComponent},
   {path:"rendezvous", component:RendezvousComponent},
-  {path:"medecin", component:MedecinComponent},
   {path:"",redirectTo:"patient",pathMatch:"full"}
 ];
 
